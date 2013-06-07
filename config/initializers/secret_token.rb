daedc9301889136dcc20eb20f4119e40873136a8
vinod <vinodkmrmurugan@gmail.com>
2013-06-07 12:54:26 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Behat::Application.config.secret_token = 'eca11858de15da996c0d475adbfc02847012c5f5a8f7db6a2e761b8fe0552cfc7c911b3f0bd9d60f9c483cc0b6ee32ed2e5a0151699789b566259357aa3f998b'
